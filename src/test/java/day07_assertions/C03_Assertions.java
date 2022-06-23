3aa10d94c83c37135ebcbf90f5c08d4b8321a474
YavuzPYRZ <yavuzpoyraz03@gmail.com>
2022-06-23 22:16:28 +0200

    23.06

---
+++ src/test/java/day07_assertions/C03_Assertions.java
@@ -0,0 +1,32 @@
+package day07_assertions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class C03_Assertions {
+
+    static WebDriver driver;
+
+    @BeforeClass
+    public static void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+    }
+
+    @AfterClass
+    public static void tearDown(){
+        driver.close();
+    }
+
+
+
+
+}
